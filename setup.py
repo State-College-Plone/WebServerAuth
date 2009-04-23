c6e84c9b6414cba84e5bab471e437e54e1e32bac
Erik Rose <ewr119@psu.edu>
2009-04-23 21:45:01 +0000

    * Bumped version to 1.3dev; 1.3 will be the first eggified release. I thought it impolite to break everybody's svn checkouts with only a hundredths-place release.  * Made the setup script strip the "dev" off the version number so it can stick it back on again, provoked by setup.cfg.  * Gave up on getting em dashes in ReStructuredText to work.  * Commented out all dependencies, as it would have wrecked everyone's day who was using pre-egg versions of them.

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 from setuptools import setup, find_packages
 import os
 
+version = open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Products', 'WebServerAuth', 'version.txt')).read().strip()
+if version.endswith('dev'):
+    version = version[:-3]
+
 setup(
     name='Products.WebServerAuth',
-    version=open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Products', 'WebServerAuth', 'version.txt')).read().strip(),
+    version=version,
     description="Delegates Plone's authentication concerns to a web server",
-    long_description="""WebServerAuth allows Plone to delegate authentication concerns to a web server like Apache or IIS. Using WebServerAuth, Plone can be configured so any user known to your LDAP, Kerberos, Shibboleth, or Pubcookie system|mdash|or any other system for which your web server has an authentication module|mdash|can transparently log in using enterprise-wide credentials.
+    long_description="""WebServerAuth allows Plone to delegate authentication concerns to a web server like Apache or IIS. Using WebServerAuth, Plone can be configured so any user known to your LDAP, Kerberos, Shibboleth, or Pubcookie system (or any other system for which your web server has an authentication module) can transparently log in using enterprise-wide credentials.
 
-`Read the whole readme. <http://plone.org/products/webserverauth/>`_
-
-.. |mdash| unicode:: U+02014 .. em dash""",
+`Read more at the WebServerAuth product page... <http://plone.org/products/webserverauth/>`_
+""",
     classifiers=[
-    "Development Status :: 5 - Production/Stable",
-    "Framework :: Plone",
-    "Framework :: Zope2",
-    "Intended Audience :: System Administrators",
-    "Programming Language :: Python",
-    "Topic :: Security",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    "Topic :: System :: Systems Administration :: Authentication/Directory"
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Intended Audience :: System Administrators",
+        "Programming Language :: Python",
+        "Topic :: Security",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Systems Administration :: Authentication/Directory"
     ],
     keywords='web zope plone authentication pas zope2',
     author='Erik Rose',
@@ -30,9 +33,15 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-      'setuptools',
-      'Products.CMFCore',
-      'Products.PluggableAuthService'
+        'setuptools',
+        # Older versions of the following weren't eggified. Including them as requirements would preclude WebServerAuth's use with them, when in fact they work.
+        # 'Products.CMFCore',
+        # 'Products.PluggableAuthService',
+        # 'Zope2'
     ],
+    extras_require={
+        # Older versions of Plone weren't eggified. Including them as requirements would preclude WebServerAuth's use with them, when in fact they work.
+        # 'Plone': ['Plone>=3.1.3']  # Plone-savvy but also works with raw Zope 2
+    },
     entry_points={}
 )
