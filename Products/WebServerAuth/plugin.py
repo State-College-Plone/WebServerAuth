4b12335330e949915ced4ec2de0a7f8b6aabbea9
nathan.vangheem <nathan.vangheem@wildcardcorp.com>
2016-03-28 14:45:39 -0500

    Fix Plone 5 CSRF issues

--- Products/WebServerAuth/plugin.py
+++ Products/WebServerAuth/plugin.py
@@ -21,6 +21,13 @@ from Products.PluggableAuthService.permissions import ManageUsers
 from Products.WebServerAuth.utils import wwwDirectory
 from Products.WebServerAuth.config import configDefaults, configDefaults1_1, configDefaults1_5, configDefaults1_6, configDefaults1_7, defaultChallengePattern, defaultChallengeReplacement, stripDomainNamesKey, stripWindowsDomainKey, usernameHeaderKey, authenticateEverybodyKey, useCustomRedirectionKey, challengePatternKey, challengeReplacementKey, cookieCheckEnabledKey, cookieNameKey, challengeHeaderEnabledKey, challengeHeaderNameKey, defaultUsernameHeader, secretHeaderKey, secretValueKey, secretEnabledKey, forceLowercaseUsernamesKey
 
+try:
+    from plone.protect.interfaces import IDisableCSRFProtection
+except:
+    IDisableCSRFProtection = None
+from zope.interface import alsoProvides
+
+
 # Key for PAS extraction dict:
 usernameKey = 'apache_username'
 
@@ -30,15 +37,14 @@ logger = logging.getLogger('Products.WebServerAuth')
 class MultiPlugin(BasePlugin):
     security = ClassSecurityInfo()
     meta_type = 'WebServerAuth Plugin'
-    
     ## PAS interface implementations: ############################
-    
+
     security.declarePublic('loginUrl')
     def loginUrl(self, currentUrl):
         """Given the URL of the page where the user presently is, return the URL which will prompt him for authentication and land him at the same place.
-        
+
         If something goes wrong, return ''.
-        
+
         """
         usingCustomRedirection = self.config[useCustomRedirectionKey]
         pattern, replacement = usingCustomRedirection and (self.config[challengePatternKey], self.config[challengeReplacementKey]) or (defaultChallengePattern, defaultChallengeReplacement)
@@ -54,7 +60,7 @@ class MultiPlugin(BasePlugin):
                 return destination
         # Our regex didn't match, or something went wrong.
         return ''
-    
+
     protocol = 'http'
     security.declarePrivate('challenge')
     def challenge(self, request, response):
@@ -62,33 +68,27 @@ class MultiPlugin(BasePlugin):
         if config[challengeHeaderEnabledKey]:
             if request.get_header(config[challengeHeaderNameKey]) is None:
                 return False
-        
+
         url = self.loginUrl(request.ACTUAL_URL)
         if url:
             response.redirect(url, lock=True)
             return True
         else:  # Pass off control to the next challenge plugin.
             return False
-    
+
     security.declarePrivate('enumerateUsers')
     # Inspired by the OpenID plugin
     def enumerateUsers(self, id=None, login=None, exact_match=False, sort_by=None, max_results=None, **kw):
         """Evil, layer-violating enumerator to get the logged in user, though unenumerable, to be validatable.
-        
+
         PAS doesn't seem to make an allowance for authorizing (IIRC) an existing user if that user cannot be enumerated, so we try to guess who's calling and make that happen.
-        
+
         """
         # Unless we're admitting non-Plone-dwelling users, don't pretend the user is there. Also, don't enumerate unless we seem to have been called by getUserById(). We're very conservative, even checking the types of things like exact_match. Also also, don't enumerate unless we're searching for the currently logged in user. Heck, we even look at the stack now, because searchUsers() (as called by searchPrincipals()) calls us the same way getUserById() does.
         if self.config[authenticateEverybodyKey] and (id is not None and login is None and exact_match is True and sort_by is None and max_results is None and not kw) and (self.REQUEST and self._normalizedLoginName(self.REQUEST.environ.get(self.config[usernameHeaderKey])) == id):  # may be redundant with the stack inspection below, but it makes me feel warm and fuzzy for now
-            # I can't find any situation in which we get here in the code and we are not called by getUserById; perhaps we should eliminate this test in a future release? (cah190)
             stack = inspect.stack()
-            calledByGetUserById = False
             try:
-                # search stack for getUserById call since hotfixes/monkey patches may vary the position in the stack
-                for frame in stack:
-                    if frame[3] == 'getUserById':
-                        calledByGetUserById = True
-                        break
+                calledByGetUserById = stack[2][3] == 'getUserById'  # getUserById calls _verifyUser, which calls us
             finally:
                 del stack  # Dispose of frames, as recommended by http://docs.python.org/lib/inspect-stack.html
             if calledByGetUserById:
@@ -102,38 +102,41 @@ class MultiPlugin(BasePlugin):
     security.declarePrivate('authenticateCredentials')
     def authenticateCredentials(self, credentials):
         """Expects a login name in the form returned by extractCredentials() below.
-    
+
         Example:
           >>> authenticateCredentials({usernameKey: 'foobar'})
           ('foobar', 'foobar')
-    
+
         """
         defaultDate = '2000/01/01'
         def setLoginTimes(member):
             """Do what the logged_in script usually does, with regard to login times, to users after they log in."""
             # Ripped off and simplified from CMFPlone.MembershipTool.MembershipTool.setLoginTimes():
             now = self.ZopeTime()
-            
+
             # Duplicate mysterious logic from MembershipTool.py:
             lastLoginTime = member.getProperty('login_time', defaultDate)  # In Plone 2.5, 'login_time' property is DateTime('2000/01/01') when a user has never logged in, so this default never kicks in. However, I'll assume it was in the MembershipTool code for a reason.
             if str(lastLoginTime) == defaultDate:
                 lastLoginTime = now
             member.setMemberProperties({'login_time': now, 'last_login_time': lastLoginTime})
-        
+
         login = credentials.get(usernameKey)
         if login is None:
             return None
         else:
             user = self._getPAS().getUser(login)  # Our enumerator doesn't respond to getUser() calls.
             userId = user and user.getId() or login
-            
+
             membershipTool = getToolByName(self, 'portal_membership', default=None)
             if membershipTool is not None:  # Tolerate running in plain Zope, sans Plone.
                 member = membershipTool.getMemberById(userId)  # works thanks to our UserEnumerationPlugin  # implicitly creates the record in portal_memberdata, at least when authenticate_everybody is on: see memberdata.py:67
-                
+
                 if member is None:  # happens only when authenticate_everybody is off
                     return None
-                
+
+                if IDisableCSRFProtection is not None:
+                    alsoProvides(self.REQUEST, IDisableCSRFProtection)
+
                 if str(member.getProperty('login_time')) == defaultDate:  # This member has never had his login time set; he's never logged in before.
                     setLoginTimes(member)  # lets the user show up in member searches. We do this only when the member record is first created. This means the login times are less accurate than in a stock Plone with form-based login, in which the times are set at each login. However, if we were to set login times at each request, that's an expensive DB write at each, and lots of ConflictErrors happen. The real answer is for somebody (Plone or PAS) to fire an event when somebody logs in.
                 membershipTool.createMemberArea(member_id=userId)
@@ -142,41 +145,41 @@ class MultiPlugin(BasePlugin):
     security.declarePrivate('extractCredentials')
     def extractCredentials(self, request):
         """Get login name from a request header passed into Zope.
-    
+
         Example:
           >>> class MockRequest:
           ...     def __init__(self, environ={}):
           ...         self.environ = environ
-    
+
           >>> request = MockRequest({'HTTP_X_REMOTE_USER': 'foobar'})
-    
+
           >>> handler = MultiPlugin('someId')
           >>> handler.extractCredentials(request)
           {'apache_username': 'foobar'}
-    
+
         """
-        
+
         # Do not extract credentials if we are configured to expect a cookie
         # but it is not found.
         if self.config[cookieCheckEnabledKey]:
             cookieName = self.config[cookieNameKey]
             if cookieName and cookieName not in request.cookies:
                 return None
-            
+
         # Do not extract credentials if a secret matching is enabled, but not matched.
         if self.config[secretEnabledKey]:
             secret = request.environ.get(self.config[secretHeaderKey])
             if secret != self.config[secretValueKey]:
                 return None
-            
+
         login = request.environ.get(self.config[usernameHeaderKey])
         if not login:
             return None
         return {usernameKey: self._normalizedLoginName(login)}
-    
-    
+
+
     ## Helper methods: ############################
-    
+
     security.declarePrivate('_normalizedLoginName')
     def _normalizedLoginName(self, login):
         """Given a raw login name, return it modified according to the "Strip domain names from usernames" preference."""
@@ -190,7 +193,7 @@ class MultiPlugin(BasePlugin):
             if self.config[forceLowercaseUsernamesKey]:
                 login = login.lower()
         return login
-    
+
     @property
     def config(self):
         """Return the configuration mapping, in the latest format."""
@@ -199,16 +202,16 @@ class MultiPlugin(BasePlugin):
             self._config = self.__dict__['config']  # sidestep descriptor
             del self.__dict__['config']
             self._config.update(configDefaults1_1)
-        
+
         # Upgrade to 1.4 format:
         if stripWindowsDomainKey not in self._config:
             self._config[stripWindowsDomainKey] = configDefaults[stripWindowsDomainKey]
-        
+
         # Upgrade to 1.5 format:
         if not isinstance(self._config, PersistentDict):
             self._config = PersistentDict(self._config)
             self._config.update(configDefaults1_5)
-        
+
         # Upgrade to 1.6 format:
         if secretEnabledKey not in self._config:
             self._config.update(configDefaults1_6)
@@ -216,12 +219,12 @@ class MultiPlugin(BasePlugin):
         # Upgrade to 1.7 format:
         if forceLowercaseUsernamesKey not in self._config:
             self._config.update(configDefaults1_7)
-            
+
         return self._config
-    
-    
+
+
     ## ZMI crap: ############################
-    
+
     def __init__(self, id, title=None):
         BasePlugin.__init__(self)
 
@@ -236,14 +239,14 @@ class MultiPlugin(BasePlugin):
     # Add a tab that calls that method:
     manage_options = ({'label': 'Options',
                        'action': 'manage_config'},) + BasePlugin.manage_options
-    
+
     security.declareProtected(ManageUsers, 'configForView')
     def configForView(self):
         """Return a mapping of my configuration values, for use in a page template."""
         ret = dict(self.config)
         ret['challenge_pattern_uncompiled'] = ret['challenge_pattern'].pattern
         return ret
-    
+
     security.declareProtected(ManageUsers, 'manage_changeConfig')
     def manage_changeConfig(self, REQUEST=None):
         """Update my configuration based on form data."""
@@ -252,7 +255,7 @@ class MultiPlugin(BasePlugin):
         for key in [usernameHeaderKey, challengeReplacementKey, cookieNameKey, challengeHeaderNameKey, secretHeaderKey, secretValueKey]:
             self.config[key] = REQUEST.form[key]
         self.config[challengePatternKey] = re.compile(REQUEST.form[challengePatternKey])
-        
+
         return REQUEST.RESPONSE.redirect('%s/manage_config' % self.absolute_url())
 
 
